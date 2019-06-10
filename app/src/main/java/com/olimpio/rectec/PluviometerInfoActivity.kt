280fead11940b51ce7272cfe40b8964768a1c4fb
Carlos Olimpio <carlossolimpio@hotmail.com>
2019-06-10 17:47:41 -0300

    top back button

--- app/src/main/java/com/olimpio/rectec/PluviometerInfoActivity.kt
+++ app/src/main/java/com/olimpio/rectec/PluviometerInfoActivity.kt
@@ -2,9 +2,11 @@ package com.olimpio.rectec
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBar
 import android.support.v7.app.AlertDialog
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_pluviometer_info.*
@@ -17,10 +19,14 @@ class PluviometerInfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pluviometer_info)
 
+        //top back button
+        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
+        getSupportActionBar()?.setDisplayShowHomeEnabled(true)
+
+
         val pluv = intent.getParcelableExtra<Pluviometer>("itemPluivometer")
         val textViewPluvId = tv_pluv_id_value
         val textViewPluvLocation = tv_pluv_location_value
-
         textViewPluvId.text = pluv.id
         textViewPluvLocation.text = pluv.location
 
@@ -28,10 +34,16 @@ class PluviometerInfoActivity : AppCompatActivity() {
         rv_measures.adapter = pluviometerInfoAdapter
         rv_measures.layoutManager = LinearLayoutManager(this)
 
+        //button click
         val btnShowMeasure = findViewById<Button>(R.id.btn_show_measure)
         btnShowMeasure.setOnClickListener {
             showAlertMeasure(pluv) //irá receber o pluv da api
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 
     private fun showAlertMeasure(pluv: Pluviometer) {
